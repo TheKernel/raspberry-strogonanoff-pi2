3a1d21c23e23b267da42149c9e30c51aa2df9c1b
root <root@living.(none)>
2015-04-26 22:52:57 +0000

    readme update

--- src/WiringPin.py
+++ src/WiringPin.py
@@ -1,6 +1,6 @@
-import wiringpi
+import wiringpi2
 
-wiringpi.wiringPiSetup()
+wiringpi2.wiringPiSetup()
 
 class WiringPin:
 
@@ -9,15 +9,15 @@ class WiringPin:
         self.direction = direction
 
     def export(self):
-        wiringpi.pinMode(self.gpio_number,
-            wiringpi.OUTPUT if self.direction == "out" else wiringpi.INPUT)
+        wiringpi2.pinMode(self.gpio_number,
+            1 if self.direction == "out" else 0)
         return self
 
     def set_value(self, value):
-        wiringpi.digitalWrite(self.gpio_number, value)
+        wiringpi2.digitalWrite(self.gpio_number, value)
 
     def get_value(self):
-        return wiringpi.digitalRead(self.gpio_number)
+        return wiringpi2.digitalRead(self.gpio_number)
 
     def unexport(self):
         pass # for now
